8023c0683cd04cbb15234b12030afdf7d3bd80cd
Sampaio Nelsandro <nelsandro.sampaio@nokia.com>
2024-11-23 23:35:40 +0100

    added function into data manager to allow communication with google sheet

--- data_manager.py
+++ data_manager.py
@@ -6,10 +6,24 @@ This class is responsible for talking to the Google Sheet.
 
 """
 
+import requests
+
 __author__ = "Nelsandro"
 __version__ = "0.1"
 
 
-
 class DataManager:
-    pass
+    def __init__(self, google_sheet_bearer_token, google_sheet_endpoint):
+        self.API_BEARER_TOKEN = google_sheet_bearer_token
+        self.API_ENDPOINT = google_sheet_endpoint
+        self.AUTH = {"Authorization": f"Bearer {self.API_BEARER_TOKEN}"}
+        self.destinations_data = self.get_destinations_and_prices()
+
+    def get_destinations_and_prices(self):
+        response = requests.get(url=self.API_ENDPOINT, headers=self.AUTH)
+        response.raise_for_status()
+        return response.json()
+
+    def update_new_lowest_price(self, destination, price):
+        # TODO: Add implementation allowing to update the new lowest prince of the next 6 months"
+        pass
